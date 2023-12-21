b420455068b3aed04962ca0db0e79966bd61c6e4
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-12-22 00:11:06 +0300

    Create main abstract class

---
+++ parsers/abc_class.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from typing import Any
+
+import httpx
+from httpx import AsyncClient
+
+
+class Parser(ABC):
+
+    @staticmethod
+    async def get_response(target_url: str | list) -> str | Any:
+        """
+        Метод для отправки HTTP запроса на стартовый URL адрес.
+        :return: Текстовое содержание HTML разметки.
+        """
+
+        async with AsyncClient() as client:
+            try:
+                response = await client.get(target_url, timeout=4)
+                response.encoding = 'utf8'
+                return response.text
+
+            except httpx.ConnectTimeout as error:
+                return error
+
+            except httpx.ReadTimeout as error:
+                return error
