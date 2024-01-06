cd318b82dc77fe048f1fd39c2b9836c2ee8abc87
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2024-01-06 17:17:00 +0300

    Calling methods after refactoring

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ async def main():
     data_parser = DataParser()
 
     watches_url = await url_parser.get_url_for_each_product_card(specific="watch")
-    # await data_parser.write_csv(watches_url, write_headers=False)
-    await data_parser.get_data_from_item_card(watches_url)
+    await data_parser.write_csv(watches_url)
 
     # all_category_url_page = await url_parser.get_url_for_each_category_page()
     # await data_parser.get_data_from_page(all_category_url_page)
